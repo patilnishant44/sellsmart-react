3cbb6db9240244774e4f65ee40202bcc940720b2
patilnishant44 <YOUR_GITHUB_EMAIL_USED_IN_PATILNISHANT44>
2025-12-10 10:28:29 +0530

    final fix: router + github pages alignment

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>   {/* ← only here */}
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
